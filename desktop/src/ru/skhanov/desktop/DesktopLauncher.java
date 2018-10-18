b80c74c943fc66e09489b3855128e81243cd4c79
skhanov <sekhanov@gmail.com>
2018-10-18 10:01:38 +0300

    initCommit

---
+++ desktop/src/ru/skhanov/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.skhanov.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.skhanov.SpaceGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceGame(), config);
+	}
+}
